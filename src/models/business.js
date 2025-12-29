826311a7fc57e4442344c05e76e4c96769e004dc
MichealHakeem2 <michealhakeem33@gmail.com>
2025-12-29 09:28:24 +0200

    first commit

---
+++ src/models/business.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+
+const businessSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+
+  description: String,
+
+  avatarUrl: String,
+
+  coverUrl: String,
+
+  type: {
+    type: String,
+    enum: ["individual", "company"],
+    required: true
+  },
+
+  industry: {
+    type: String,
+    required: true
+  },
+
+  companySize: {
+    type: String,
+    enum: ["startup", "shop", "small", "medium", "enterprise"],
+    default: "startup"
+  },
+
+  website: String,
+
+  verified: {
+    type: Boolean,
+    default: false
+  },
+
+  ownerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+
+  managers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }],
+
+  followersCount: {
+    type: Number,
+    default: 0
+  },
+
+  reputationScore: {
+    type: Number,
+    default: 0
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Business", businessSchema);
